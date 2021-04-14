92fc2b532c6c50f2e4cf1936eff0b83227271a21
randomuserhi <magicalworld.nano@gmail.com>
2021-04-14 23:57:49 +0100

    Completed

--- Slime/Assets/Particles.cs
+++ Slime/Assets/Particles.cs
@@ -1,41 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
-
-static class CanvasExtensions
-{
-    public static Vector2 SizeToParent(this RawImage Image, float Padding = 0)
-    {
-        var Parent = Image.transform.parent.GetComponentInParent<RectTransform>();
-        var Transform = Image.GetComponent<RectTransform>();
-        if (!Parent) { return Transform.sizeDelta; } //if we don't have a parent, just return our current width
-        Padding = 1 - Padding;
-        float Ratio = Image.texture.width / (float)Image.texture.height;
-        var Bounds = new Rect(0, 0, Parent.rect.width, Parent.rect.height);
-        if (Mathf.RoundToInt(Transform.eulerAngles.z) % 180 == 90)
-        {
-            //Invert the bounds if the image is rotated
-            Bounds.size = new Vector2(Bounds.height, Bounds.width);
-        }
-        //Size by height first
-        float Height = Bounds.height * Padding;
-        float Width = Height * Ratio;
-        if (Width > Bounds.width * Padding)
-        { //If it doesn't fit, fallback to width
-            Width = Bounds.width * Padding;
-            Height = Width / Ratio;
-        }
-        Transform.sizeDelta = new Vector2(Width, Height);
-        return Transform.sizeDelta;
-    }
-}
 
 public class Particles : MonoBehaviour
 {
     RenderTexture Texture;
     RenderTexture DiffuseTexture;
-    public RawImage Render;
+    RenderTexture DisplayTexture;
+    public Texture2D EnvironmentTexture;
     public ComputeShader Shader;
     public ComputeBuffer ParticleBuffer;
 
@@ -54,6 +26,8 @@ public class Particles : MonoBehaviour
 
     void Start()
     {
+        Application.runInBackground = true;
+
         Texture = new RenderTexture(1920, 1080, 24);
         Texture.enableRandomWrite = true;
         Texture.filterMode = FilterMode.Point;
@@ -64,7 +38,14 @@ public class Particles : MonoBehaviour
         DiffuseTexture.filterMode = FilterMode.Point;
         DiffuseTexture.Create();
 
-        ParticleList = new Particle[2500];
+        DisplayTexture = new RenderTexture(1920, 1080, 24);
+        DisplayTexture.enableRandomWrite = true;
+        //DisplayTexture.filterMode = FilterMode.Point;
+        DisplayTexture.Create();
+
+        transform.GetComponentInChildren<MeshRenderer>().material.mainTexture = DisplayTexture;
+
+        ParticleList = new Particle[500000];
 
         ParticleBuffer = new ComputeBuffer(ParticleList.Length, Particle.Size());
         ParticleBuffer.SetData(ParticleList);
@@ -78,30 +59,32 @@ public class Particles : MonoBehaviour
         Shader.SetTexture(0, "Result", Texture);
         Shader.SetTexture(1, "Result", Texture);
         Shader.SetTexture(4, "Result", Texture);
+        Shader.SetTexture(0, "EnvironmentMap", EnvironmentTexture);
+        Shader.SetTexture(4, "EnvironmentMap", EnvironmentTexture);
         Shader.SetTexture(4, "DiffusedMap", DiffuseTexture);
 
-        Shader.SetTexture(2, "Result", Texture);
-        Shader.Dispatch(2, Texture.width / 8 + Texture.width % 8, Texture.height / 8 + Texture.height % 8, 1);
+        //Shader.SetTexture(2, "EnvironmentMap", EnvironmentTexture);
+        //Shader.Dispatch(2, EnvironmentTexture.width / 8 + EnvironmentTexture.width % 8, EnvironmentTexture.height / 8 + EnvironmentTexture.height % 8, 1);
         Shader.Dispatch(3, ParticleList.Length / 16 + ParticleList.Length % 16, 1, 1);
-
-        Render.texture = DiffuseTexture;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (Render != null) Render.SizeToParent();
-    }
-
-    float TotalTime;
     void FixedUpdate()
     {
-        Shader.SetFloat("Time", TotalTime);
-        //Shader.Dispatch(1, Texture.width / 8 + Texture.width % 8, Texture.height / 8 + Texture.height % 8, 1);
-        Shader.Dispatch(0, ParticleList.Length / 16 + ParticleList.Length % 16, 1, 1);
-        Shader.Dispatch(4, DiffuseTexture.width / 8 + DiffuseTexture.width % 8, DiffuseTexture.height / 8 + DiffuseTexture.height % 8, 1);
-        Graphics.Blit(DiffuseTexture, Texture);
-        TotalTime += Time.fixedDeltaTime;
+        for (int i = 0; i < 2; i++)
+        {
+            Shader.SetFloat("Time", Time.fixedTime);
+            //Shader.Dispatch(1, Texture.width / 8 + Texture.width % 8, Texture.height / 8 + Texture.height % 8, 1);
+            Shader.Dispatch(0, ParticleList.Length / 16 + ParticleList.Length % 16, 1, 1);
+            Shader.Dispatch(4, DiffuseTexture.width / 8 + DiffuseTexture.width % 8, DiffuseTexture.height / 8 + DiffuseTexture.height % 8, 1);
+            Graphics.Blit(DiffuseTexture, Texture);
+        }
+    }
+
+    public Material Transparency;
+
+    private void LateUpdate()
+    {
+        Graphics.Blit(Texture, DisplayTexture);
     }
 
     private void OnApplicationQuit()
